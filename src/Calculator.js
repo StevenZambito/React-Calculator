891ce5acacd0aecf1ed9fcae5b0760928a0d309e
Steven Zambito <zambitosteven@gmail.com>
2020-08-19 18:46:05 -0400

    Enhance calculator design

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,34 +5,34 @@ export const Calculator = () => {
 	return (
 		<div className={styles.CalculatorShell}>
 			<div className={styles.screenSection}>
-				<p className={styles.screen}></p>
+				<p className={styles.screen}>0</p>
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.meow}>Clear</button>
-				<button className={styles.meow}>/</button>
+				<button className={styles.calcButton}>Clear</button>
+				<button className={styles.calcButton}>/</button>
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.meow}>7</button>
-				<button className={styles.meow}>8</button>
-				<button className={styles.meow}>9</button>
-				<button className={styles.meow}>x</button>
+				<button className={styles.calcButton}>7</button>
+				<button className={styles.calcButton}>8</button>
+				<button className={styles.calcButton}>9</button>
+				<button className={styles.calcButton}>x</button>
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.meow}>4</button>
-				<button className={styles.meow}>5</button>
-				<button className={styles.meow}>6</button>
-				<button className={styles.meow}>-</button>
+				<button className={styles.calcButton}>4</button>
+				<button className={styles.calcButton}>5</button>
+				<button className={styles.calcButton}>6</button>
+				<button className={styles.calcButton}>-</button>
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.meow}>1</button>
-				<button className={styles.meow}>2</button>
-				<button className={styles.meow}>3</button>
-				<button className={styles.meow}>+</button>
+				<button className={styles.calcButton}>1</button>
+				<button className={styles.calcButton}>2</button>
+				<button className={styles.calcButton}>3</button>
+				<button className={styles.calcButton}>+</button>
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.meows}>0</button>
-				<button className={styles.meow}>.</button>
-				<button className={styles.meow}>=</button>
+				<button className={styles.calcButtonTwo}>0</button>
+				<button className={styles.calcButton}>.</button>
+				<button className={styles.calcButton}>=</button>
 			</div>
 		</div>
 	);
