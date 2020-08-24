fa81b486f249e11f20520aad044fee9c4daf6017
Steven Zambito <zambitosteven@gmail.com>
2020-08-24 16:45:07 -0400

    Make results and button components

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,38 +1,38 @@
 import React from "react";
 import styles from "./Calculator.module.css";
+import { Results } from "./Results.js";
+import { Button } from "./Button.js";
 
 export const Calculator = () => {
 	return (
 		<div className={styles.CalculatorShell}>
-			<div className={styles.screenSection}>
-				<p className={styles.screen}>0</p>
+			<Results />
+			<div className={styles.hey}>
+				<Button />
+				<Button />
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.calcButton}>Clear</button>
-				<button className={styles.calcButton}>÷</button>
+				<Button />
+				<Button />
+				<Button />
+				<Button />
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.calcButton}>7</button>
-				<button className={styles.calcButton}>8</button>
-				<button className={styles.calcButton}>9</button>
-				<button className={styles.calcButton}>x</button>
+				<Button />
+				<Button />
+				<Button />
+				<Button />
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.calcButton}>4</button>
-				<button className={styles.calcButton}>5</button>
-				<button className={styles.calcButton}>6</button>
-				<button className={styles.calcButton}>-</button>
+				<Button />
+				<Button />
+				<Button />
+				<Button />
 			</div>
 			<div className={styles.hey}>
-				<button className={styles.calcButton}>1</button>
-				<button className={styles.calcButton}>2</button>
-				<button className={styles.calcButton}>3</button>
-				<button className={styles.calcButton}>+</button>
-			</div>
-			<div className={styles.hey}>
-				<button className={styles.calcButtonTwo}>0</button>
-				<button className={styles.calcButton}>.</button>
-				<button className={styles.calcButton}>=</button>
+				<Button />
+				<Button />
+				<Button />
 			</div>
 		</div>
 	);
---
+++ src/Results.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Results.module.css";
+
+export const Results = () => {
+	return (
+		<div className={styles.screenSection}>
+			<p className={styles.screen}>0</p>
+		</div>
+	);
+};
